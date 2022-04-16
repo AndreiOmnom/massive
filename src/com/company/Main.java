81d51bcdf49e64b60642c2bfd83af53ba6873f75
AndreiOmnom <belcargoru@gmail.com>
2022-04-16 19:37:10 +0300

    упр.2 отсортировать массив

---
+++ src/com/company/Main.java
@@ -0,0 +1,67 @@
+package com.company;
+
+import com.company.Arrayservice.ArrayService;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = {10, 556, 4, 39, 17};
+
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array, 0, 5);
+        System.out.println(Arrays.toString(array));
+
+//        Scanner scanner = new Scanner(System.in);
+//        int len = scanner.nextInt();
+//        String[] nums = new String[len];
+        //      int len2 = scanner.nextInt();
+        //     String[] nums2 = new String[len2];
+//        for (int i = 0; i < len; i++) {
+//            nums[i] = scanner.next();
+        //    for (int i = 0; i < len2; i++) {
+        //       nums2[i] = scanner.next();
+//            }
+//        ArrayService.Sortirovka(nums, len);
+        //     ArrayService.findShortestLongest(nums, len);
+    }
+}
+
+//            ArrayService.thirdArray(nums,nums2, len);
+     //   }
+//        int max = MagicMath.max(45,67);
+//        System.out.println(max);
+//        int powa = MagicMath.pow (2, 5);
+//        System.out.println(powa);
+//        int abs = MagicMath.abs (18);
+//        System.out.println(abs);
+//        int round = MagicMath.round (23.34);
+//        System.out.println(round);
+
+
+//        Pet jack =  new Pet(  "Jack",  "Buldog", true, 'M', 10  );
+//
+//
+//        jack.age  = 10;
+//        jack.name = "Jack";
+//        jack.pol = 'M';
+//        jack.breed = "Buldog";
+//        jack.isAngry = true;
+//
+//        System.out.println(jack);
+//        jack.growPet();
+//        System.out.println(jack.age);
+//
+//        Food meat = new Food(10);
+//        Food junk = new Food(0);
+//        jack.feed(meat);
+//        System.out.println(jack);
+//
+//
+//
+//    }
+ //   }
